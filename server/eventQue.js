b2414dcd28b7075631ccc4d6b956037d0df4c663
Paul Kassianik <pkassianik@gmail.com>
2015-08-05 20:42:05 -0700

    Temp fix for callback not firing

--- server/eventQue.js
+++ server/eventQue.js
@@ -83,6 +83,7 @@ module.exports = function(v_session_id) {
 							var newCoordinates = [currentValue.new_coordinates_x, currentValue.new_coordinates_y]
 							console.log(currentValue.client_id+" "+currentValue.new_coordinates_x+" "+currentValue.new_coordinates_y)
 							sqlHandler.moveClient(currentValue.client_id, newCoordinates, callback)
+							callback()
 							console.log("hi-in-switch")
 							break;
 						default:
